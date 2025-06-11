9d2f20771bf32f1bae5d80201076118d02de0e48
eduardovsousa <eduardo.varela@outlook.com.br>
2025-06-11 17:36:35 -0300

    feat: create data table for agents

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,14 +1,18 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
     const [existingAgent] = await db
-      .select()
+      .select({
+        //TODO: alterar para contagem real
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
       .from(agents)
       .where(eq(agents.id, input.id))
 
