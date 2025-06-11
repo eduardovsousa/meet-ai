cadfe0bf3c38ccf989502008f8320b5a9017dbb7
eduardovsousa <eduardo.varela@outlook.com.br>
2025-06-11 17:41:19 -0300

    fix: ensure function getMany on agentsRouter get meetingCount

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -20,7 +20,11 @@ export const agentsRouter = createTRPCRouter({
   }),
   getMany: protectedProcedure.query(async () => {
     const data = await db
-      .select()
+      .select({
+        //TODO: alterar para contagem real
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
       .from(agents);
 
     return data;
