7b77fe63edeb9d2ba0f5728857600816c10f4db1
eduardovsousa <eduardo.varela@outlook.com.br>
2025-06-13 14:23:18 -0300

    feat: sync RSC filters and client filters, mofidy agents.getMany procedure, add nuqs, add ui for filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import type { SearchParams } from "nuqs";
 
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
@@ -8,9 +9,17 @@ import { ErrorBoundary } from "react-error-boundary";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { loadSearchParams } from "@/modules/agents/params";
+
 import { auth } from "@/lib/auth";
 
-const Page = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -20,7 +29,9 @@ const Page = async () => {
   }
 
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    ...filters,
+  }));
 
   return (
     <>
