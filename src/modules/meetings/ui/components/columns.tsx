37741058540400d0810c7306823b56a6d7c8a56d
eduardovsousa <eduardo.varela@outlook.com.br>
2025-06-24 16:02:41 -0300

    feat: add summary tab and recording tab

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { format } from "date-fns"
-import humanizeDuration from "humanize-duration"
 import { ColumnDef } from "@tanstack/react-table"
 import {
   CircleCheckIcon,
@@ -16,15 +15,7 @@ import { GeneratedAvatar } from "@/components/generated-avatar"
 import { Badge } from "@/components/ui/badge"
 
 import { MeetingGetMany } from "../../types"
-import { cn } from "@/lib/utils"
-
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"],
-  });
-};
+import { cn, formatDuration } from "@/lib/utils"
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
